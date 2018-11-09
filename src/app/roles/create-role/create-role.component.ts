a1693ef1b24c377c57c65e5ada49534b0bd67eed
José Polanco <josepolanco18@outlook.com>
2018-11-09 19:01:59 +0100

    Commit

--- src/app/roles/create-role/create-role.component.ts
+++ src/app/roles/create-role/create-role.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild, Injector, Output, EventEmitter, ElementRef, OnInit } from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap';
-import { RoleServiceProxy, CreateRoleDto, ListResultDtoOfPermissionDto, EstudianteServiceProxy } from '@shared/service-proxies/service-proxies';
+import { RoleServiceProxy, CreateRoleDto, ListResultDtoOfPermissionDto } from '@shared/service-proxies/service-proxies';
 import { AppComponentBase } from '@shared/app-component-base';
 import { finalize } from 'rxjs/operators';
 
